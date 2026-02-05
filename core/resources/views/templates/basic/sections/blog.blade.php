34b991c02f1d0821110a9dcfa6bb58e74599d42d
Emilson de Souza Nascimento <emilsonsn2@gmail.com>
2026-02-05 18:31:10 -0300

    fix: corrige blog da página home

--- core/resources/views/templates/basic/sections/blog.blade.php
+++ core/resources/views/templates/basic/sections/blog.blade.php
@@ -20,22 +20,33 @@
                         <div class="blog-card__thumb">
                             <img src="{{ frontendImage('blog', @$blog->data_values->image, '430x280', thumb: true) }}">
                         </div>
-                        <div class="blog-card__content">
-                            <h4 class="blog-card__title mb-3">
-                                <a href="{{ route('blog.details', $blog->slug) }}">
-                                    {{ __(strLimit(@$blog->data_values->title, 60)) }}
-                                </a>
-                            </h4>
-                            <ul class="blog-card__meta d-flex mb-4 flex-wrap">
-                                <li>
-                                    <i class="las la-calendar"></i>
-                                    {{ showDateTime($blog->created_at) }}
-                                </li>
-                            </ul>
-                            <a class="btn btn--base mt-3 see-more" href="{{ route('blog.details', $blog->slug) }}">
-                                @lang('Read More') <i class="las la-long-arrow-alt-right"></i>
+                    <div class="blog-card__content">
+                        <h4 class="blog-card__title mb-3">
+                            <a href="{{ route('blog.details', $blog->slug) }}">
+                                {{ __(strLimit(@$blog->data_values->title, 60)) }}
                             </a>
-                        </div>
+                        </h4>
+                        @php
+                            $raw = $blog->data_values->description_nic
+                                ?? $blog->data_values->description
+                                ?? $blog->data_values->details
+                                ?? '';
+
+                            // blog.element content may be a full HTML document (pdf2htmlEX). Strip noisy parts.
+                            $raw = preg_replace('~<head\\b[^>]*>.*?</head>~is', '', (string) $raw);
+                            $raw = preg_replace('~<script\\b[^>]*>.*?</script>~is', '', (string) $raw);
+                            $raw = preg_replace('~<style\\b[^>]*>.*?</style>~is', '', (string) $raw);
+
+                            $excerpt = trim(html_entity_decode(strip_tags($raw)));
+                            $excerpt = preg_replace('/\\s+/', ' ', $excerpt);
+                        @endphp
+                        @if ($excerpt)
+                            <p class="mb-3">{{ __(strLimit($excerpt, 140)) }}</p>
+                        @endif
+                        <a class="btn btn--base mt-3 see-more" href="{{ route('blog.details', $blog->slug) }}">
+                            @lang('Read More') <i class="las la-long-arrow-alt-right"></i>
+                        </a>
+                    </div>
                     </div>
                 </div>
             @endforeach
